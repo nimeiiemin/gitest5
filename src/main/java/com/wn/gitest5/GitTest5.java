30ef90d739528ae69f2ca36212dee548a32373cb
wangXin <awake.silent@qq.com>
2022-08-20 12:25:42 +0800

    master comnit1

---
+++ src/main/java/com/wn/gitest5/GitTest5.java
@@ -0,0 +1,9 @@
+package com.wn.gitest5;
+
+public class GitTest5 {
+    public static void main(String[] args) {
+        System.out.println("git1");
+        System.out.println("git2");
+        System.out.println("git3");
+    }
+}
